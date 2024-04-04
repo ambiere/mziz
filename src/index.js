193a8c0be36b26af4d5036fc316430da20ab3f07
zhid0399123 <zhid0399123@gmail.com>
2024-04-04 20:44:33 +0300

    module, get project root dir

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import fs from "node:fs";
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const cwd = path.dirname(fileURLToPath(import.meta.url))
+
+export default function projectRootDir(dir = cwd) {
+  const git = path.join(dir, ".git")
+  const pkg = path.join(dir, 'package.json');
+  if (fs.existsSync(pkg) && fs.existsSync(git)) {
+    return dir;
+  }
+  if (path.resolve(dir, '..') === dir) {
+    throw new Error('Could not find package.json or .git in the directory tree. :(');
+  } else {
+    return getRootDir(path.resolve(dir, '..'));
+  }
+}
+
